78275503386e0661c15927f01992b1d7b84951d2
ayushgoyal04 <ayushgoyal434@gmail.com>
2024-03-17 12:10:56 +0530

    sorting

---
+++ Basics and Concepts/Sorting/MergeSort.cpp
@@ -0,0 +1,42 @@
+// merging two sorted array
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n;
+    int a1[n];
+    for(int i = 0; i < n; i++) {
+        cin >> a1[i];
+    }
+
+    cin >> m;
+    int a2[m];
+    for(int i = 0; i < m; i++) {
+        cin >> a2[i];
+    }
+
+    int i = 0, j = 0, k = 0;
+    int arr[n + m];
+
+    while(i < n && j < m) {
+        if(a1[i] < a2[j]) {
+            arr[k++] = a1[i++];
+        } else {
+            arr[k++] = a2[j++];
+        }
+    }
+
+    while(i < n) {
+        arr[k++] = a1[i++];
+    }
+    while(j < m) {
+        arr[k++] = a2[j++];
+    }
+
+    for(int i = 0; i < n + m; i++) {
+        cout << arr[i] << " ";
+    }    
+
+    return 0;
+}
